f7803283e3cffd95fc18761854a015df038d0711
Amirmohammad Abedini <Amirmohammad.Abedini@gmail.com>
2019-06-06 00:52:03 +0430

    radio needs parham's magic touch

--- NewsSiteFrontEnd/ClientApp/src/components/Radio.js
+++ NewsSiteFrontEnd/ClientApp/src/components/Radio.js
@@ -3,14 +3,39 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 export class Radio extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            urls: [{}],
+            b: false
+        };
+    }
+    getHtml() {
+        if (this.state.b) {
+            return (<a onClick={this.state.b = true} > 1</a>);
+        }
+        
+        return (<ReactAudioPlayer
+            src="https://localhost:44335/radio"
+            autoPlay
+            controls
+            loop
+        />);
+    }
+    componentWillMount() {
+        axios.get("https://localhost:44335/radio/tracks").then(res => this.setState({ urls: res.data }));
+    }
     render() {
         return (
-            <div><ReactAudioPlayer
-                src="https://localhost:44335/radio"
-            autoPlay
-                controls
-                loop
-            />
-                </div>);
+            <div>
+
+                {this.getHtml()}
+            </div>
+
+
+
+        );
     };
 };
